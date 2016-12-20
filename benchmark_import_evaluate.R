9ec97b5e576f867b0274712f85f0be101eaa0d4c
Nelson Stauffer <ngs@nmsu.edu>
2016-12-20 10:51:16 -0700

    Converted benchmark import to a function

--- benchmark_import_evaluate.R
+++ benchmark_import_evaluate.R
@@ -19,25 +19,43 @@ tdat <- tdat.spdf@data
 
 indicator.lut <- read.csv(paste0(data.path, "/", tdat.indicators.lut), stringsAsFactors = F)
 
-## Import the spreadsheet from the workbook. Should work regardless of presence/absence of other spreadsheets as long as the name is the same
-benchmarks.raw <- read.xlsx(file = paste0(data.path, "/", benchmarks.filename),
-                    sheetName = "Monitoring Objectives",
-                    header = T,
-                    stringsAsFactors = F)
 
-## Strip out the extraneous columns and rows, which includes if they left the example in there
-benchmarks <- benchmarks.raw[!grepl(x = benchmarks.raw$Management.Question, pattern = "^[Ee].g.") & !is.na(benchmarks.raw$Indicator), 1:12]
+## Function for reading in the benchmarks from the Data Explorer
+read.benchmarks <- function(data.path = "", ## Path to the folder containing the Data Explorer with the benchmarks in it
+                            benchmarks.filename = "", ## The filename of the Data Explorer workbook
+                            indicator.lut ## A lookup table with a column called "indicator.name" matching the values in the Data Explorer "Indicator" field and one called "indicator.tdat" with corresponding value for the indicators' names in TerrADat
+                            ){
+  ## Sanitizing inputs because users can't be trusted
+  if (!grepl(x = data.path, pattern = "/$")) {
+    data.path <- paste0(data.path, "/")
+  }
+  if (!grepl(x = benchmarks.filename, pattern = "\\.[Xx][Ll][Ss][Xx]$")) {
+    benchmarks.filename <- paste0(benchmarks.filename, ".xlsx")
+  }
+    ## Import the spreadsheet from the workbook. Should work regardless of presence/absence of other spreadsheets as long as the name is the same
+  benchmarks.raw <- read.xlsx(file = paste0(data.path, benchmarks.filename),
+                              sheetName = "Monitoring Objectives",
+                              header = T,
+                              stringsAsFactors = F)
+  
+  ## Strip out the extraneous columns and rows, which includes if they left the example in there
+  benchmarks <- benchmarks.raw[!grepl(x = benchmarks.raw$Management.Question, pattern = "^[Ee].g.") & !is.na(benchmarks.raw$Indicator), 1:12]
+  
+  ## Create the evaluations for the upper and lower limits of each benchmark.
+  ## The way the spreadsheet is configured, there should be no rows without both defined
+  benchmarks$eval.string.lower <- paste0(benchmarks$Lower.Limit, benchmarks$LL.Relation)
+  benchmarks$eval.string.upper <- paste0(benchmarks$UL.Relation, benchmarks$Upper.Limit)
+  
+  ## Create an evaluation string for future use with the required proportion and its relationship
+  benchmarks$eval.string.proportion <- paste0(benchmarks$Proprtion.Relation, benchmarks$Required.Proportion)
+  
+  ## For each benchmark add in the name of the field in TerrADat that corresponds
+  benchmarks <- merge(x = benchmarks, y = indicator.lut, by.x = "Indicator", by.y = "indicator.name")
+  
+  return(benchmarks)
+}
 
-## Create the evaluations for the upper and lower limits of each benchmark.
-## The way the spreadsheet is configured, there should be no rows without both defined
-benchmarks$eval.string.lower <- paste0(benchmarks$Lower.Limit, benchmarks$LL.Relation)
-benchmarks$eval.string.upper <- paste0(benchmarks$UL.Relation, benchmarks$Upper.Limit)
-
-## Create an evaluation string for future use with the required proportion and its relationship
-benchmarks$eval.string.proportion <- paste0(benchmarks$Proprtion.Relation, benchmarks$Required.Proportion)
-
-## For each benchmark add in the name of the field in TerrADat that corresponds
-benchmarks <- merge(x = benchmarks, y = indicator.lut, by.x = "Indicator", by.y = "indicator.name")
+benchmarks <- read.benchmarks(data.path, benchmarks.filename, indicator.lut)
 
 ## Slicing the data frame from terradat.spdf
 ## This step depends on how you're attributing the points with evaluation strata.
